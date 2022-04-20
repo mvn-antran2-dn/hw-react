fb283e40a50fb3576b32239a86853ded90363e2a
mvn-antran2-dn <an.tran2@monstar-lab.com>
2022-04-20 09:55:45 +0700

    update: Bt4 react

---
+++ hw-react2/src/Component/about.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class About extends Component{
+  render(){
+    return(
+      <>
+      <h2 className="About">About</h2>
+      </>
+    )
+  };
+}
+
+export default About;
